edb0db345922afb4f1d2474fc95e2b7c3424e86d
Vitalygorkov <vet55omsk@gmail.com>
2021-01-05 00:21:48 +0600

    file models.py created

--- models.py
+++ models.py
@@ -1,40 +1,13 @@
-from sqlalchemy import Column, Integer, Unicode, UnicodeText, String
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+import sqlite3
 
-from random import choice
-from string import letters
-
-engine = create_engine('sqlite:////tmp/teste.db', echo=True)
-Base = declarative_base(bind=engine)
-
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    name = Column(Unicode(40))
-    address = Column(UnicodeText, nullable=True)
-    password = Column(String(20))
-
-    def __init__(self, name, address=None, password=None):
-        self.name = name
-        self.address = address
-        if password is None:
-            password = ''.join(choice(letters) for n in xrange(10))
-        self.password = password
-
-Base.metadata.create_all()
-
-Session = sessionmaker(bind=engine)
-s = Session()
-
-# create instances of my user object
-u = User('nosklo')
-u.address = '66 Some Street #500'
-
-u2 = User('lakshmipathi')
-u2.password = 'ihtapimhskal'
-
-# testing
-s.add_all([u, u2])
-s.commit()
+conn = sqlite3.connect('bazasearch.db')
+cur = conn.cursor()
+# cur.execute("SELECT * FROM channels;")
+# all_results = cur.fetchall()
+cur.execute("""CREATE TABLE IF NOT EXISTS vidos(
+   User TEXT PRIMARY KEY,
+   Goals TEXT,
+   prosm TEXT,
+   Others TEXT);
+""")
+conn.commit()
