f26f571875a0b75d9465ecc152a224e5e3bef4d0
Vitalygorkov <vet55omsk@gmail.com>
2021-01-04 21:18:17 +0600

    file models.py created

--- time_management_bot.py
+++ time_management_bot.py
@@ -5,18 +5,15 @@ bot = telebot.TeleBot(api_key)
 
 keyboard1 = telebot.types.ReplyKeyboardMarkup()
 keyboard1.row('баланс', 'банк')
-
+print('start')
 @bot.message_handler(content_types=['text'])
 def start(message):
     if message.text.lower() == 'баланс':
         bot.send_message(message.from_user.id, 'Баланс', reply_markup=keyboard1)
-
         print('Баланс')
 
     else:
         print(message.text)
-
         bot.send_message(message.from_user.id, 'Задача пробел минуты', reply_markup=keyboard1)
-
 
 bot.polling(none_stop=True, interval=0)
