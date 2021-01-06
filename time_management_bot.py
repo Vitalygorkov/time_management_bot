e009dc3687547bc3bc81f18adabd5d9c170ed1be
Vitalygorkov <vet55omsk@gmail.com>
2021-01-06 23:03:42 +0600

    Job Adding Command Processing Template

--- time_management_bot.py
+++ time_management_bot.py
@@ -1,5 +1,7 @@
 import telebot
 from config import api_key
+import time
+
 
 bot = telebot.TeleBot(api_key)
 
@@ -8,12 +10,15 @@ keyboard1.row('баланс', 'банк')
 print('start')
 @bot.message_handler(content_types=['text'])
 def start(message):
-    if message.text.lower() == 'баланс':
-        bot.send_message(message.from_user.id, 'Баланс', reply_markup=keyboard1)
+    if message.text.lower() == 'Go':
+        bot.send_message(message.from_user.id, 'начинаем эффективную работу', reply_markup=keyboard1)
         print('Баланс')
+    elif message.text.lower().split()[0].isdigit() == True:
+        bot.send_message(message.from_user.id, 'задание '+ message.text, reply_markup=keyboard1)
+        print('задание')
 
     else:
         print(message.text)
-        bot.send_message(message.from_user.id, 'Задача пробел минуты', reply_markup=keyboard1)
+        bot.send_message(message.from_user.id, 'Минуты пробел задача', reply_markup=keyboard1)
 
 bot.polling(none_stop=True, interval=0)
