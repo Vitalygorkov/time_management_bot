70646cf0b57850aff9f5c80738d677792a849a58
Vitalygorkov <vet55omsk@gmail.com>
2021-01-08 20:47:13 +0600

    added job handlers to time_management_bot.py and created an example class

--- models.py
+++ models.py
@@ -1,13 +1,23 @@
 import sqlite3
 
-conn = sqlite3.connect('bazasearch.db')
-cur = conn.cursor()
-# cur.execute("SELECT * FROM channels;")
-# all_results = cur.fetchall()
-cur.execute("""CREATE TABLE IF NOT EXISTS vidos(
-   User TEXT PRIMARY KEY,
-   Goals TEXT,
-   prosm TEXT,
-   Others TEXT);
-""")
-conn.commit()
+class TaskObject(object):
+
+    def __init__(self, name, status, type):
+        """Constructor"""
+        self.name = name
+        self.status = status
+        self.type = type # three types of tasks - ten-year, annual, monthly
+
+
+
+# conn = sqlite3.connect('tm_base.db')
+# cur = conn.cursor()
+# # cur.execute("SELECT * FROM channels;")
+# # all_results = cur.fetchall()
+# cur.execute("""CREATE TABLE IF NOT EXISTS vidos(
+#    User TEXT PRIMARY KEY,
+#    Goals TEXT,
+#    prosm TEXT,
+#    Others TEXT);
+# """)
+# conn.commit()
--- time_management_bot.py
+++ time_management_bot.py
@@ -6,17 +6,55 @@ import time
 bot = telebot.TeleBot(api_key)
 
 keyboard1 = telebot.types.ReplyKeyboardMarkup()
-keyboard1.row('баланс', 'банк')
+keyboard1.row('задача', 'банк')
 print('start')
 @bot.message_handler(content_types=['text'])
 def start(message):
     if message.text.lower() == 'Go':
         bot.send_message(message.from_user.id, 'начинаем эффективную работу', reply_markup=keyboard1)
         print('Баланс')
+    elif message.text.lower().split()[0] == '1':
+        the_task = message.text.split()
+        print(the_task)
+        the_task.remove('1')
+        print(the_task)
+        the_task = ' '.join(the_task)
+        bot.send_message(message.from_user.id, 'Добавлена задача на 10 лет: ' + the_task)
+
+    elif message.text.lower().split()[0] == '2':
+        the_task = message.text.split()
+        print(the_task)
+        the_task.remove('2')
+        print(the_task)
+        the_task = ' '.join(the_task)
+        bot.send_message(message.from_user.id, 'Добавлена задача на год: ' + the_task)
+
+    elif message.text.lower().split()[0] == '3':
+        the_task = message.text.split()
+        print(the_task)
+        the_task.remove('3')
+        print(the_task)
+        the_task = ' '.join(the_task)
+        bot.send_message(message.from_user.id, 'Добавлена задача на месяц: ' + the_task)
+
+    elif message.text.lower().split()[0] == '4':
+        the_task = message.text.split()
+        print(the_task)
+        the_task.remove('4')
+        print(the_task)
+        the_task = ' '.join(the_task)
+        bot.send_message(message.from_user.id, 'Добавлена задача на день: ' + the_task)
+
     elif message.text.lower().split()[0].isdigit() == True:
         bot.send_message(message.from_user.id, 'задание '+ message.text, reply_markup=keyboard1)
         print('задание')
-
+    elif message.text.lower().split()[0] == 'задача':
+        the_task = message.text.lower().split()
+        print(the_task)
+        the_task.remove('задача')
+        print(the_task)
+        the_task = ' '.join(the_task)
+        bot.send_message(message.from_user.id, 'Добавлена задача: ' + the_task)
     else:
         print(message.text)
         bot.send_message(message.from_user.id, 'Минуты пробел задача', reply_markup=keyboard1)
