414fbea2cc4525254e5f4e17b422498516207104
Vitalygorkov <vet55omsk@gmail.com>
2021-01-12 23:34:19 +0600

    Creation of a multilevel menu with buttons. So far I have made a prototype for 2 menus

--- time_management_bot.py
+++ time_management_bot.py
@@ -5,65 +5,117 @@ import models
 bot = telebot.TeleBot(api_key)
 
 keyboard1 = telebot.types.ReplyKeyboardMarkup()
-keyboard1.row('задача', 'банк')
+keyboard1.row('add', 'del', 'complete')
 print('start')
+
+
+# заготовка с менюшками и кнопками
+
 @bot.message_handler(content_types=['text'])
 def start(message):
-    if message.text.lower() == 'Go':
-        bot.send_message(message.from_user.id, 'начинаем эффективную работу', reply_markup=keyboard1)
-        print('Баланс')
-    elif message.text.lower().split()[0] == '1':
-        the_task = message.text.split()
-        print(the_task)
-        the_task.remove('1')
-        print(the_task)
-        the_task = ' '.join(the_task)
-        bot.send_message(message.from_user.id, 'Добавлена задача на 10 лет: ' + the_task)
+    keyboard = telebot.types.ReplyKeyboardMarkup(True)
+    keyboard.row('Первое меню', 'Второе меню')
+    msg = bot.send_message(message.chat.id, text = 'Нажми кнопку в меню', reply_markup = keyboard )
+    menu1 = telebot.types.InlineKeyboardMarkup()
+    menu1.add(telebot.types.InlineKeyboardButton(text = 'Первая кнопка первого меню', callback_data ='first menu1'))
+    menu1.add(telebot.types.InlineKeyboardButton(text = 'Вторая кнопка первого меню', callback_data ='second menu1'))
+    menu1.add(telebot.types.InlineKeyboardButton(text = 'третья кнопка первого меню', callback_data='тhree menu1'))
 
-    elif message.text.lower().split()[0] == '2':
-        the_task = message.text.split()
-        print(the_task)
-        the_task.remove('2')
-        print(the_task)
-        the_task = ' '.join(the_task)
-        bot.send_message(message.from_user.id, 'Added task for a year: ' + the_task)
-
-    elif message.text.lower().split()[0] == '3':
-        the_task = message.text.split()
-        print(the_task)
-        the_task.remove('3')
-        print(the_task)
-        the_task = ' '.join(the_task)
-        bot.send_message(message.from_user.id, 'Added a task for a month: ' + the_task)
-
-    elif message.text.lower().split()[0] == '4':
-        the_task = message.text.split()
-        print(the_task)
-        the_task.remove('4')
-        print(the_task)
-        the_task = ' '.join(the_task)
-        models.task_write(the_task,0,4)
-        bot.send_message(message.from_user.id, 'Added daily task: ' + the_task)
-
-    elif message.text.lower().split()[0].isdigit() == True:
-        bot.send_message(message.from_user.id, 'task '+ message.text, reply_markup=keyboard1)
-        print('task')
-    elif message.text.lower().split()[0] == 'task':
-        the_task = message.text.lower().split()
-        print(the_task)
-        the_task.remove('task')
-        print(the_task)
-        the_task = ' '.join(the_task)
-        bot.send_message(message.from_user.id, 'Added task: ' + the_task)
-    else:
-        print(message.text)
-        bot.send_message(message.from_user.id, '1-4(task type) space then task). Tasks for one day: ', reply_markup=keyboard1)
-        tasks = ''
-        num = 1
-        for i in models.task_read(4):
-            tasks += str(num) + '. '+ i[0] + '\n'
-            num +=1
-        bot.send_message(message.from_user.id, tasks, reply_markup=keyboard1)
+    menu2 = telebot.types.InlineKeyboardMarkup()
+    menu2.add(telebot.types.InlineKeyboardButton(text = 'Первая кнопка второго меню', callback_data ='first menu2'))
+    menu2.add(telebot.types.InlineKeyboardButton(text = 'Вторая кнопка второго меню', callback_data ='second menu2'))
+    menu2.add(telebot.types.InlineKeyboardButton(text = 'третья кнопка второго меню', callback_data='тhree menu2'))
 
 
+    if message.text == 'Первое меню':
+        msg = bot.send_message(message.chat.id, text='Нажми первую inline кнопку', reply_markup=menu1)
+    elif message.text == 'Второе меню':
+        msg = bot.send_message(message.chat.id, text='Нажми первую inline кнопку', reply_markup=menu2)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def step2(call):
+    menu2call = telebot.types.InlineKeyboardMarkup()
+    menu2call.add(telebot.types.InlineKeyboardButton(text = 'Третья кнопка', callback_data ='third'))
+    menu2call.add(telebot.types.InlineKeyboardButton(text = 'Четвертая кнопка', callback_data ='fourth'))
+
+    if call.data == 'first':
+        msg = bot.send_message(call.message.chat.id, 'Нажми третью кнопку', reply_markup = menu2call)
+    elif call.data == 'third':
+        msg = bot.send_message(call.message.chat.id, 'Конец')
+
+
+
+
+# предыдущий код который переносим в заготовку
+# @bot.message_handler(content_types=['text'])
+# def start(message):
+#     if message.text.lower() == 'add':
+#         bot.send_message(message.from_user.id, 'вы хотите добавить задачу, напишите цифру типа задачи нажмите пробел и напишите задачу', reply_markup=keyboard1)
+#         print('Баланс')
+#         if message.text.lower().split()[0] == '4':
+#             the_task = message.text.split()
+#             print(the_task)
+#             the_task.remove('4')
+#             print(the_task)
+#             the_task = ' '.join(the_task)
+#             models.task_write(the_task,0,4)
+#             bot.send_message(message.from_user.id, 'Added daily task: ' + the_task)
+#
+#     # elif message.text.lower().split()[0] == '1':
+#     #     the_task = message.text.split()
+#     #     print(the_task)
+#     #     the_task.remove('1')
+#     #     print(the_task)
+#     #     the_task = ' '.join(the_task)
+#     #     bot.send_message(message.from_user.id, 'Добавлена задача на 10 лет: ' + the_task)
+#     #
+#     # elif message.text.lower().split()[0] == '2':
+#     #     the_task = message.text.split()
+#     #     print(the_task)
+#     #     the_task.remove('2')
+#     #     print(the_task)
+#     #     the_task = ' '.join(the_task)
+#     #     bot.send_message(message.from_user.id, 'Added task for a year: ' + the_task)
+#     #
+#     # elif message.text.lower().split()[0] == '3':
+#     #     the_task = message.text.split()
+#     #     print(the_task)
+#     #     the_task.remove('3')
+#     #     print(the_task)
+#     #     the_task = ' '.join(the_task)
+#     #     bot.send_message(message.from_user.id, 'Added a task for a month: ' + the_task)
+#     # # если в начале предложения 4
+#     # elif message.text.lower().split()[0] == '4':
+#     #     the_task = message.text.split()
+#     #     print(the_task)
+#     #     the_task.remove('4')
+#     #     print(the_task)
+#     #     the_task = ' '.join(the_task)
+#     #     models.task_write(the_task,0,4)
+#     #     bot.send_message(message.from_user.id, 'Added daily task: ' + the_task)
+#
+#     # elif message.text.lower().split()[0].isdigit() == True:
+#     #     bot.send_message(message.from_user.id, 'task '+ message.text, reply_markup=keyboard1)
+#     #     print('task')
+#     elif message.text.lower().split()[0] == 'task':
+#         the_task = message.text.lower().split()
+#         print(the_task)
+#         the_task.remove('task')
+#         print(the_task)
+#         the_task = ' '.join(the_task)
+#         bot.send_message(message.from_user.id, 'Added task: ' + the_task)
+#
+#     # выводим задачи на день
+#     else:
+#         print(message.text)
+#         bot.send_message(message.from_user.id, '1-4(task type) space then task). Tasks for one day: ', reply_markup=keyboard1)
+#         tasks = ''
+#         num = 1
+#         for i in models.task_read(4):
+#             tasks += str(num) + '. '+ i[0] + '\n'
+#             num +=1
+#         bot.send_message(message.from_user.id, tasks, reply_markup=keyboard1)
+#
+#
 bot.polling(none_stop=True, interval=0)
